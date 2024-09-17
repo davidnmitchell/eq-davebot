88b2fb3d7ae2c669d77cc84a24d9de442fd1e03e
davidnmitchell <david.n.mitchell@gmail.com>
2024-09-17 10:20:13 -0600

    Added events from self

--- teameventbot.lua
+++ teameventbot.lua
@@ -36,6 +36,10 @@ local function callback_pull_start(line, who, mob)
 	end
 end
 
+local function callback_pull_start_me(line, mob)
+	callback_pull_start(line, mq.TLO.Me.Name(), mob)
+end
+
 local function callback_pull_end(line, who, mob)
 	if group.IsPuller(who) then
 		local cmd = str.Trim(Config:TeamEvents():OnPullEnd() or '')
@@ -43,6 +47,10 @@ local function callback_pull_end(line, who, mob)
 			mq.cmd(cmd)
 		end
 	end
+end
+
+local function callback_pull_end_me(line, mob)
+	callback_pull_end(line, mq.TLO.Me.Name(), mob)
 end
 
 local function callback_pre_engage(line, who, mob)
@@ -54,6 +62,10 @@ local function callback_pre_engage(line, who, mob)
 	end
 end
 
+local function callback_pre_engage_me(line, mob)
+	callback_pre_engage(line, mq.TLO.Me.Name(), mob)
+end
+
 local function callback_engaging(line, who, mob)
 	if group.IsMainAssist(who) then
 		local cmd = str.Trim(Config:TeamEvents():OnEngage() or '')
@@ -61,6 +73,10 @@ local function callback_engaging(line, who, mob)
 			mq.cmd(cmd)
 		end
 	end
+end
+
+local function callback_engaging_me(line, mob)
+	callback_engaging(line, mq.TLO.Me.Name(), mob)
 end
 
 
@@ -74,12 +90,16 @@ function teameventbot.Init(cfg)
 
 	mq.event('teamevent1', "#1# tells the group, 'Pulling #2#'", callback_pull_start)
 	mq.event('teamevent1.1', "#1# tells the group, in #*#, 'Pulling #2#'", callback_pull_start)
+	mq.event('teamevent1.2', "You tell your party, 'Pulling #1#'", callback_pull_start_me)
 	mq.event('teamevent2', "#1# tells the group, 'Arrived back at camp'", callback_pull_end)
 	mq.event('teamevent2.1', "#1# tells the group, in #*#, 'Arrived back at camp'", callback_pull_end)
+	mq.event('teamevent2.2', "You tell your party, 'Arrived back at camp'", callback_pull_end_me)
 	mq.event('teamevent3', "#1# tells the group, 'Waiting to engage #2#'", callback_pre_engage)
 	mq.event('teamevent3.1', "#1# tells the group, in #*#, 'Waiting to engage #2#'", callback_pre_engage)
+	mq.event('teamevent3.2', "You tell your party, 'Waiting to engage #1#'", callback_pre_engage_me)
 	mq.event('teamevent4', "#1# tells the group, 'Engaging #2#'", callback_engaging)
 	mq.event('teamevent4.1', "#1# tells the group, in #*#, 'Engaging #2#'", callback_engaging)
+	mq.event('teamevent4.2', "You tell your party, 'Engaging #1#'", callback_engaging_me)
 
 	log('Initialized')
 end
