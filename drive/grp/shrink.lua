4c8048fce6d727071be327b4357ed410e3eef792
davidnmitchell <david.n.mitchell@gmail.com>
2024-11-27 12:15:46 -0700

    Fixed for both Shaman and Beastlord uses

--- drive/grp/shrink.lua
+++ drive/grp/shrink.lua
@@ -9,47 +9,49 @@ local MyClass = EQClass:new()
 
 local function name_of_caster()
     local i = group.FirstOfClass('Shaman')
+    local spell_key = 'shrink15'
     if i < 0 then
         i = group.FirstOfClass('Beastlord')
+        if i < 0 then
+            return '', ''
+        end
+        spell_key = 'shrink23'
     end
-    if i < 0 then
-        return ''
-    end
-    return mq.TLO.Group.Member(i).Name()
+    return mq.TLO.Group.Member(i).Name(), spell_key
 end
 
-local function send_spell(caster, target_id)
-    mq.cmd('/squelch /bct ' .. caster .. ' //drive cast -spell|shrink15 -target|' .. target_id)
+local function send_spell(caster, spell_key, target_id)
+    mq.cmd('/squelch /bct ' .. caster .. ' //drive cast -spell|' .. spell_key .. ' -target|' .. target_id)
 end
 
 local function shrink_me()
-    local caster = name_of_caster()
+    local caster, spell_key = name_of_caster()
     if caster:len() > 0 then
-        send_spell(caster,  mq.TLO.Me.ID())
+        send_spell(caster, spell_key, mq.TLO.Me.ID())
     end
 end
 
 local function shrink_name(name)
-    local caster = name_of_caster()
+    local caster, spell_key = name_of_caster()
     local target_id = mq.TLO.Spawn(name).ID()
     if caster:len() > 0 and target_id ~= nil and target_id > 0 then
-        send_spell(caster, target_id)
+        send_spell(caster, spell_key, target_id)
     end
 end
 
 local function shrink_all()
-    local caster = name_of_caster()
+    local caster, spell_key = name_of_caster()
     for i, id in ipairs(group.IDs()) do
-        send_spell(caster, id)
+        send_spell(caster, spell_key, id)
     end
 end
 
 local function shrink_bigs()
-    local caster = name_of_caster()
+    local caster, spell_key = name_of_caster()
     for i=0, mq.TLO.Group.GroupSize()-1 do
         print(mq.TLO.Group.Member(i).Name() .. ':' .. mq.TLO.Group.Member(i).Height())
         if mq.TLO.Group.Member(i).Height() >= 4.0 then
-            send_spell(caster, mq.TLO.Group.Member(i).ID())
+            send_spell(caster, spell_key, mq.TLO.Group.Member(i).ID())
         end
     end
 end
